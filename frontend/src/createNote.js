87251a3574526bda357f6cbd194f23b1f63b9f7a
Aryanna Anderson <tuh05038@temple.edu>
2020-03-28 23:04:13 -0400

    "Created "create note" page. Has form and input boxes for title, content, and tags. On submit checks to make sure has necessary fields written in and calls function to handle request. Also created hook to empty input after form is submitted

---
+++ frontend/src/createNote.js
@@ -0,0 +1,96 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import React from "react";
+import "./createNoteStyle.css";
+
+//hook created to handle resetting values to empty after submission of note
+import { useInput } from "./inputHook";
+
+
+//function that takes input and sends it to server 
+function makeRequest(title, content, tags) {
+  console.log(title);
+  console.log(content);
+  console.log(tags);
+  //parse tags string by , and put values into an array
+  tags = tags.split(" ").join("");
+  let tagsList = tags.split(",");
+  console.log(tagsList);
+  //create unique id
+  //make request 
+}
+
+export default function NoteForm() {
+  //initializing title, content, and tag values
+  const { value: Title, bind: bindTitle, reset: resetTitle } = useInput("");
+  const { value: Content, bind: bindContent, reset: resetContent } = useInput("");
+  const { value: Tags, bind: bindTags, reset: resetTags } = useInput("");
+
+  
+    //event handler for onsubmit 
+    const handleSubmit = evt => {
+    evt.preventDefault();
+    //checking to see if title was entered
+    if (Title === "") {
+      alert(`Error: note needs a title`);
+      return;
+    }
+    //checking to see if content was entered
+    else if (Content === "") {
+      alert(`Error: note needs content`);
+      return;
+    } 
+    //sending note to function that will handle request
+    else {
+      alert(`Submitting Note: ${Title}`);
+      makeRequest(Title, Content, Tags);
+    }
+    //resetting content in input boxes
+    resetTitle();
+    resetContent();
+    resetTags();
+  };
+  
+  
+  //returning note form
+  return (
+    <div className="form">
+        <h1> Create New Note</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+        <span>Title:</span>
+          <input
+            className="title"
+            type="text"
+            {...bindTitle}
+            placeholder="Title"
+          />
+        </label>
+        <label>
+        <span>Content:</span>
+          <textarea
+            className="content"
+            rows="20"
+            cols="30"
+            type="text"
+            {...bindContent}
+          />
+        </label>
+        <label>
+        <span>Tags (seperate tags by commas):</span>
+          <input
+            className="tags"
+            type="text"
+            {...bindTags}
+            placeholder="Add tags"
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NoteForm from './createNote';
 
 ReactDOM.render(
     <React.StrictMode>
         <App />
+        <NoteForm />
     </React.StrictMode>,
     document.getElementById("root")
 );
