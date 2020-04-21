89d88f262ecee50687b31f92863e7ae175fad177
Matthew Cohen <mcohen0365@gmail.com>
2020-04-21 04:09:47 -0400

    added more improvements for small layouts

--- frontend/src/components/NotesEditor/ContentArea.js
+++ frontend/src/components/NotesEditor/ContentArea.js
@@ -11,29 +11,29 @@ import './ContentArea.css';
 import { NoteContext } from '../../contexts/NoteContext';
 
 function handleUpdateErrors(response) {
-        if (!response.ok) {
-            console.log("error calling api");
-            alert("note could not be updated");
-        }
-        return response;
-    };
+    if (!response.ok) {
+        console.log("error calling api");
+        alertify.error('Note could not be updated');
+    }
+    return response;
+};
     
-    function handleDeleteErrors(response) {
-        if (!response.ok) {
-            console.log("error calling api");
-            alert("note could not be deleted");
-        }
-        return response;
-    };
+function handleDeleteErrors(response) {
+    if (!response.ok) {
+        console.log("error calling api");
+        alertify.error('Note could not be deleted');
+    }
+    return response;
+};
     
     
 function ContentArea() {
     const [note, setNote, filterNotes, filterResults, fetchNotes] = useContext(NoteContext);
-
-    const [title, setTitle] = useState("");
+    const [noteSaving, setNoteSaving] = useState(false);
 
     const saveNote = () => {
         // call api to update current note and hide the update button
+        setNoteSaving(true);
         console.log("save the current working note in the context");
         let data = {"noteTitle": note.title,
             "tags": note.tags,
@@ -52,12 +52,12 @@ function ContentArea() {
         }).then(handleUpdateErrors)
                 .then(response => response.json())
                 .then(data => {
-                    alert("note has been saved");
+                    alertify.success("Note has been saved");
                     console.log(data);
                     // updates the local copy of notes
                     fetchNotes();
                     return data;
-                });
+                }).finally(() => {setNoteSaving(false)});
     };
     
     const deleteNote = () => {
@@ -145,14 +145,11 @@ function ContentArea() {
 
     return(note !== undefined ? (
             <div className="content-area">
-                <div className="update-title">
-                    <input type="text" value={note.title} disabled={true} />
-                    <i onClick={editTitle} className="material-icons">edit</i>
-                </div>
                 
-                {/* <h2>{note.title} <i className="material-icons update-title">edit</i></h2> */}
+                
+                <h2>{note.title}</h2>
                 <MarkdownViewer />
-                <MarkdownEditor />
+                <MarkdownEditor saveNote={saveNote} />
                 <div className="tags-container">
                     <p>Tags: </p>
                     {note.tags.map((tag, index) => (
@@ -163,7 +160,12 @@ function ContentArea() {
                 {/* update and delete buttons */}
                 <div className="buttons">
                     <button onClick={addTag} className="btn-tag">Add Tag</button>
-                    <button onClick={saveNote} className="btn-save">Save</button>
+                    {noteSaving ? (
+                        <button onClick={saveNote} className="btn-save" disabled={noteSaving}>Saving...</button>
+                    ) : (
+                        <button onClick={saveNote} className="btn-save" disabled={noteSaving}>Save</button>
+                    )}
+                    
                     <button className="btn-delete" onClick={deleteNote}>delete</button>
                 </div>
             </div>
--- frontend/src/components/NotesPanel/SideNavbar.js
+++ frontend/src/components/NotesPanel/SideNavbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import {Modal, Button} from 'react-bootstrap';
 import FilterNotes from './FilterNotes';
 import NotesContainer from './NotesContainer';
@@ -13,36 +13,34 @@ function SideNavbar() {
 
     const [hideSidebar, setHideSidebar] = useState(false);
 
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            // make sure the sidebar is open
+            if (window.innerWidth > 750) {
+                setHideSidebar(false);
+                sidebarRef.current.style.position = "relative";
+                sidebarRef.current.style.visibility = "visible";
+                sidebarNavRef.current.classList.remove('sidebar-nav-hide');
+            }
+        });
+    }, []);
+
     const showNote = () => {
         var note = document.getElementById("form");
         note.style.display = "block";
         return;
     };
 
-    const handleHelpWindow = () => {
-        setShowHelpModal(!showHelpModal);
-    };
-
     const toggleSidebar = () => {
-        console.log('toggle navbar');
-        const mobileBtn = document.getElementById("menuBtn");
+        // only allow the sidebar to close on mobile whena note is clicked
+        if (window.innerWidth > 750) return;
 
-        if (window.getComputedStyle(mobileBtn).display === "none"){
-            console.log("mobile menu btn hidden");
-            return;
-        } else {
-            console.log("mobile menu btn not hidden", mobileBtn.style.display);
-        }
-
-        // console.log(sidebarRef.current.classList);
+        // hides and shows the sidebar when the mobile button is clicked
         if (!hideSidebar) {
             sidebarNavRef.current.classList.add('sidebar-nav-hide');
             setHideSidebar(true);
             sidebarRef.current.style.position = "absolute";
             sidebarRef.current.style.visibility = "hidden";
-            setTimeout(() => {
-                
-            }, 500);
         } else {
             setHideSidebar(false);
             sidebarRef.current.style.position = "relative";
@@ -52,7 +50,7 @@ function SideNavbar() {
     }
 
     return(
-        <div>
+        <div className="sidebar-container">
             <i onClick={toggleSidebar} id="menuBtn" className="material-icons">menu</i>
             <div ref={sidebarRef} className="sidebar">
                 <div ref={sidebarNavRef} className="sidebar-nav">
@@ -61,10 +59,10 @@ function SideNavbar() {
                     <FilterNotes />
                     <div className="nav-buttons">
                         <button onClick={showNote}>Create Note</button>
-                        <button onClick={handleHelpWindow}>Help</button>
+                        <button onClick={() => setShowHelpModal(true)}>Help</button>
                         
                         {/* Help button Modal */}
-                        <Modal show={showHelpModal}>
+                        <Modal show={showHelpModal} onHide={() => setShowHelpModal(false)}>
                             <Modal.Header closeButton>
                                 <Modal.Title>How to use Notes Taker++</Modal.Title>
                             </Modal.Header>
@@ -72,14 +70,16 @@ function SideNavbar() {
                                 <ul>
                                     <li>To create a note, click <strong>create note</strong> fillout the form</li>
                                     <li>The content of a note should be entered in Markdown. <a target="_blank" href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet">Here is a helpful Markdown Cheatsheet</a></li>
-                                    <li>To search for a note by content, simply start typing in the search field. To search by tags, check the
+                                    <li>To <strong>search</strong> for a note by content, simply start typing in the search field. To search by tags, check the
                                         <strong> filter by tags</strong> box and continue typing
                                     </li>
-                                    <li>Notes can be viewed by clicking on them in the left pane</li>
+                                    <li>Notes can be <strong>viewed</strong> by clicking on them in the left pane</li>
+                                    <li>Changes made to a note is <strong>only</strong> saved once the <strong>save</strong> button is clicked</li>
+                                    <li>To delete a note, simply press the <strong>delete</strong> button when viewing a note</li>
                                 </ul>
                             </Modal.Body>
                             <Modal.Footer>
-                                <Button variant="primary" onClick={handleHelpWindow}>Close</Button>
+                                <Button variant="primary" onClick={() => setShowHelpModal(false)}>Close</Button>
                             </Modal.Footer>
                         </Modal>
                     </div>
